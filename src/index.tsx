3d19cc0b7ad21a19935b2c7426c51579382ba59a
Istvan Acs <istvan.acs89@gmail.com>
2020-02-02 11:47:07 +0000

    The useState hook and the type alias

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import ReactDOM from 'react-dom';
 
+// create alias for long named types
+type FormEl = React.FormEvent<HTMLFormElement>;
+
 export default function App(): JSX.Element {
-  return <h1>Hello!!</h1>;
+  const [value, setValue] = useState<string>(''); // we can set what the value will be by default
+  // console.log(value);
+  // setValue('test');
+  // console.log(value);
+  // debugger;
+
+  const handleSubmit = (e: FormEl): void => {
+    // prevents the page to be refreshing
+    e.preventDefault();
+
+    // sets back the value to be empty after submit
+    setValue('');
+  };
+
+  return (
+    <Fragment>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type='submit'>Add Todo</button>
+      </form>
+    </Fragment>
+  );
 }
 
 const root = document.getElementById('app-root');
