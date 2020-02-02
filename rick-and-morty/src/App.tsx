1713f4169bf8aeae25eedc898b6ba268f7eac4c4
Istvan Acs <istvan.acs89@gmail.com>
2020-02-02 15:30:44 +0000

    Add some style to the cards

--- rick-and-morty/src/App.tsx
+++ rick-and-morty/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useContext, useEffect } from 'react';
 import { Store } from './Store';
+import './index.css';
 
 export default function App() {
   const { state, dispatch } = useContext(Store);
@@ -26,6 +27,20 @@ export default function App() {
     <Fragment>
       <h1>Rick and Morty</h1>
       <p>Pick your favourite episode!</p>
+      {state.episodes.map((episode: any) => (
+        <div key={episode.id} className='card'>
+          <img
+            src={episode.image.medium}
+            alt={`Rick and Morty ${episode.name}`}
+          />
+          <div className='p10 flex column'>
+            <div className='card-title'>{episode.name}</div>
+            <div className='card-desc'>
+              Season: {episode.season} Episode: {episode.number}
+            </div>
+          </div>
+        </div>
+      ))}
     </Fragment>
   );
 }
