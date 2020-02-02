521c1049d8f729eb6c051bdf09c5124d2c14f27c
Istvan Acs <istvan.acs89@gmail.com>
2020-02-02 14:19:23 +0000

    Creating our reducer

--- rick-and-morty/src/Store.tsx
+++ rick-and-morty/src/Store.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 interface IState {
   episodes: [];
   favourites: [];
 }
 
+interface IAction {
+  type: string;
+  payload: any;
+}
+
 const initialState: IState = {
   episodes: [],
   favourites: []
 };
+
 export const Store = React.createContext<IState>(initialState);
 
-function reducer() {
-  // pass
+function reducer(state: IState, action: IAction): IState {
+  switch (action.type) {
+    case 'FETCH_DATA':
+      // get the current state and put the episodes inside
+      return { ...state, episodes: action.payload };
+    default:
+      return state;
+  }
 }
 
 export function StoreProvider(props: any): JSX.Element {
+  const [state, dispatch] = useReducer(reducer, initialState);
   // anything inside a component will go through inside
 
   // goal here to make sure this value gets passed down into any component that is passed throug the store
